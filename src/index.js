4130f25a0155c12363884230254c2b0e3ae5d0a4
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-15 01:21:37 +1000

    re-setup apollo-client using @apollo/client

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from "@apollo/client";
 
-const httpLink = createHttpLink({
+const httpLink = new HttpLink({
   uri: "http://localhost:4000"
 });
 
