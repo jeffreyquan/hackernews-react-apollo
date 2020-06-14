6f8cee757a043f45e740e6866e82a1eddf47dd73
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-15 02:02:47 +1000

    created add post mutation and used useMutation hook

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,10 +1,38 @@
 import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
+
+const ADD_POST = gql`
+  mutation AddPost($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
 
 export const CreateLink = () => {
+  const [addPost, { data }] = useMutation(ADD_POST);
   const [state, setState] = useState({
     description: '',
     url: ''
   })
+
+  const handleClick = (e) => {
+    addPost({
+      variables: {
+        description: state.description,
+        url: state.url
+      }
+    })
+
+    setState({
+      description: '',
+      url: ''
+    })
+  };
 
   return (
     <div>
@@ -30,7 +58,7 @@ export const CreateLink = () => {
           placeholder="The URL for the link"
         />
       </div>
-      <button>Submit</button>
+      <button onClick={handleClick}>Submit</button>
     </div>
   )
 }
