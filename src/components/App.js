0b9f2fe13ab1b0b9ee3c585c4cb594839a78d09a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-15 23:01:21 +1000

    added login route

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,22 @@ import { Switch, Route } from "react-router-dom";
 import '../styles/App.css';
 import { CreateLink } from "./CreateLink";
 import { LinkList } from "./LinkList";
+import { Login } from "./Login";
 import Header from "./Header";
 
 function App() {
   return (
-  <div className="center w85">
-    <Header />
-    <div className="ph3 pv1 background-gray">
-      <Switch>
-        <Route exact path="/" component={LinkList} />
-        <Route exact path="/create" component={CreateLink} />
-      </Switch>
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default App;
