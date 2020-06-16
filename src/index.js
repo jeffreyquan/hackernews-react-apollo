ca32d081bb284c590a74e8e703065de269acd3ec
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-16 22:20:05 +1000

    add token to every request sent by apollo client

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
+import { setContext } from "apollo-link-context";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { ApolloClient } from "apollo-boost";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { AUTH_TOKEN } from "./constants";
+
+import './styles/index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
