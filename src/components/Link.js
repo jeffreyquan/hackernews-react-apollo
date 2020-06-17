a40e09bd607d11cbf38bdb53811abfdcd22823d6
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-17 22:43:20 +1000

    added vote mutation and on click function

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,17 +1,49 @@
 import React from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
 import { AUTH_TOKEN } from "../constants";
 import { timeDifferenceForDate } from "../utils";
 
 const authToken = localStorage.getItem(AUTH_TOKEN);
 
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
 export const Link = ({ index, link }) => {
+
+  const [voteMutation] = useMutation(VOTE_MUTATION);
+
+  const voteForLink = () => {
+    voteMutation({
+      variables: {
+        linkId: link.id
+      }
+    })
+  }
     
   return (
     <div className="flex mt2 items-start">
       <div className="flex items-center">
         <span className="gray">{index + 1}.</span>
         {authToken && (
-          <div className="ml1 gray f11" onClick={() => this._voteForLink()}>
+          <div className="ml1 gray f11" onClick={voteForLink}>
             ▲
           </div>
         )}
