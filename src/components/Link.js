cdabf2aab9d60eeab67f7949ee8f9b15dd863812
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-17 22:31:09 +1000

    conditional rendering for links. upvotes show for those signed in

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,31 @@
 import React from "react";
+import { AUTH_TOKEN } from "../constants";
+import { timeDifferenceForDate } from "../utils";
 
-export const Link = ({ link }) => {
+const authToken = localStorage.getItem(AUTH_TOKEN);
+
+export const Link = ({ index, link }) => {
+    
   return (
-    <div>
-      <div>
-        {link.description} ({link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <div className="ml1 gray f11" onClick={() => this._voteForLink()}>
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        <div className="f6 lh-copy gray">
+          {link.votes.length} votes | by{" "}
+          {link.postedBy ? link.postedBy.name : "Unknown"}{" "}
+          {timeDifferenceForDate(link.createdAt)}
+        </div>
       </div>
     </div>
-  )
+  );
 }
