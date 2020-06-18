e5c9bb1784aa7f9805e45dc2f01698019319f5dc
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-18 15:26:18 +1000

    created a search component with the feed query with a filter parameter

---
+++ src/components/Search.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { withApollo } from "react-apollo";
+import { Link } from "./Link";
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Search = props => {
+
+  const [state, setState] = useState({
+    links: [],
+    filter: ''
+  })
+
+  const executeSearch = async () => {
+    const { filter } = state;
+    const result = await props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter },
+    })
+    const links = result.data.feed.links;
+    setState({
+      ...state,
+      links
+    })
+  }
+
+  return (
+    <div>
+      <div>
+        Search
+        <input 
+          type="text" 
+          onChange={e => setState({...state, filter: e.target.value })}
+        />
+        <button onClick={executeSearch}>OK</button>
+      </div>
+      {state.links.map((link, index) => (
+        <Link key={link.id} link={link} index={index} />
+      ))}
+    </div>
+  )
+}
+
+export default withApollo(Search);
